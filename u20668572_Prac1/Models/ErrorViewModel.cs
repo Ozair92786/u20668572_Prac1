62b36df315b44742fc0ec62dff715b6bbe96b75e
ozair <ozair@DESKTOP-4K9CEIN>
2024-03-09 11:13:45 +0200

    Add project files.

---
+++ u20668572_Prac1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace u20668572_Prac1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
